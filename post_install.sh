2889918ff5f43b7caa05e0a18e9705bd547f3dd1
Nacer-BKRM <30437348+Nacer-BKRM@users.noreply.github.com>
2019-09-29 02:54:20 +0200

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 #https://mail-tp.fareoffice.com/java/jdk-8u151-linux-x64.rpm
-echo "installing java" > /tmp/post_install.log
+echo "installing wget java" > /tmp/post_install.log
 date >> /tmp/post_install.log
-yum install -y java-1.8.0-openjdk
-echo "java installed ?" >> /tmp/post_install.log
+yum install -y wget
+rpm -Uvh https://mail-tp.fareoffice.com/java/jdk-8u151-linux-x64.rpm
+echo "java wget installed ?" >> /tmp/post_install.log
 date >> /tmp/post_install.log
